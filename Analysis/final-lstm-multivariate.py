fce09813729ced3c4ac6d23f7921ef092c84f224
Timonflogo <timon.godt@live.com>
2021-05-27 09:22:06 +0200

    added files for NN

--- Analysis/final-lstm-multivariate.py
+++ Analysis/final-lstm-multivariate.py
@@ -48,6 +48,13 @@ df.isna().sum()
 df_input = df[['Datetime', 'cross_cafe', 'cloud_cover', 'humidity', 'precip_dur_past1h', 'precip_past1h', 'temp_dew',
                'temp_dry', 'temp_max_past1h', 'temp_mean_past1h', 'temp_min_past1h', 'wind_dir', 'wind_max_per10min_past1h',
                'wind_speed', 'wind_speed_past1h', 'sun_last1h_glob', 'pressure']]
+date_time = df_input.pop('Datetime')
+
+# outlier detection 
+#df_input.describe()
+# boxplot 
+#sns.boxplot(data=df_input)
+
 
 pd.set_option('display.max_columns', None) 
 # df_input.describe() # looks alright 
@@ -66,6 +73,7 @@ df_input.fillna(method='ffill', inplace = True)
 df_input['cloud_cover'].fillna(0, inplace = True)
 df_input.isna().sum()
 
+
 # data exploration 
 # add hour column
 df_input['hour'] = df_input.index.hour
@@ -83,17 +91,19 @@ df_input['is_weekend'] = ((df_input.index.dayofweek) // 5 == 1).astype(float)
 # plot heatmap
 # df_input = df_input.reset_index()
 # get correlations
-df_input_corr = df_input.corr()
-# create mask
-mask = np.triu(np.ones_like(df_input_corr, dtype=np.bool))
-
-sns.heatmap(df_input_corr, mask=mask, annot=True, fmt=".2f", cmap='Blues',
-            vmin=-1, vmax=1, cbar_kws={"shrink": .8})
+# =============================================================================
+# df_input_corr = df_input.corr()
+# # create mask
+# mask = np.triu(np.ones_like(df_input_corr, dtype=np.bool))
+# 
+# sns.heatmap(df_input_corr, mask=mask, annot=True, fmt=".2f", cmap='Blues',
+#             vmin=-1, vmax=1, cbar_kws={"shrink": .8})
+# =============================================================================
 
 # drop values 
 df_input = df_input[['cross_cafe', 'cloud_cover', 'humidity', 'pressure',
-                     'temp_mean_past1h', 'wind_max_per10min_past1h', 'sun_last1h_glob',
-                     'hour', 'day_of_week', 'is_weekend']]
+                     'temp_mean_past1h', 'wind_max_per10min_past1h', 'sun_last1h_glob', 
+                     'hour', 'day_of_month', 'day_of_week', 'month', 'is_weekend']]
 df_input.info()
 
 
@@ -119,12 +129,35 @@ df_input.info()
 
 # create final dataframe for LSTM
 df_input = df_input[['cross_cafe', 'cloud_cover', 'humidity', 'pressure',
-                     'temp_mean_past1h', 'wind_max_per10min_past1h', 'sun_last1h_glob',
-                     'hour', 'day_of_week', 'is_weekend']]
+                     'temp_mean_past1h', 'wind_max_per10min_past1h', 'sun_last1h_glob']]
 df_input.info()
+    
+# create daily, weekly, and yearly signals 
+timestamp_s = date_time.map(datetime.datetime.timestamp)
+day = 24*60*60
+week = 24*60*60*7
+year = (365.2425)*day
+
+df_input['Day sin'] = np.sin(timestamp_s * (2 * np.pi / day))
+df_input['Day cos'] = np.cos(timestamp_s * (2 * np.pi / day))
+
+
+plt.plot(np.array(df_input['Day sin'])[:25])
+plt.plot(np.array(df_input['Day cos'])[:25])
+plt.xlabel('Time [h]')
+plt.title('Time of day signal')
+
+
+# df_input.to_csv('final_dataset.csv')
+
+# =============================================================================
+# ax = sns.violinplot(x='Column', y='Normalized', data=df_input)
+# _ = ax.set_xticklabels(df_input.keys(), rotation=90)
+# =============================================================================
+
 
 # dataframe for univariate 
-# df_input = df_input['cross_cafe']
+df_input = df_input['cross_cafe']
 
 # ================================== Multi Step single shot forecasting models ========================
 
@@ -245,6 +278,7 @@ def plot(self, model=None, plot_col='cross_cafe', max_subplots=3, title = 'plot'
   max_n = min(max_subplots, len(inputs))
   for n in range(max_n):
     plt.subplot(max_n, 1, n+1)
+    plt.tight_layout(pad=1.0)
     plt.ylabel(f'{plot_col} [norm]')
     plt.plot(self.input_indices, inputs[n, :, plot_col_index],
              label='Inputs', marker='.', zorder=-10)
@@ -339,14 +373,17 @@ WindowGenerator.example = example
 # define forecasting length
 lags = 24
 OUT_STEPS = 24
-multi_window = WindowGenerator(input_width=lags, label_width=OUT_STEPS, shift=OUT_STEPS,
-                     label_columns=['cross_cafe'])
-multi_window
+multi_window = WindowGenerator(input_width=lags,
+                               label_width=OUT_STEPS,
+                               shift=OUT_STEPS,
+                               label_columns=['cross_cafe'])
+# multi_window.plot()
+# multi_window
 
 # ------------------- create compile and fit -------------------------------
 MAX_EPOCHS = 100
 
-def compile_and_fit(model, window, patience=5):
+def compile_and_fit(model, window, patience=10):
   early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss',
                                                     patience=patience,
                                                     mode='min')
@@ -357,7 +394,8 @@ def compile_and_fit(model, window, patience=5):
 
   history = model.fit(window.train, epochs=MAX_EPOCHS,
                       validation_data=window.val,
-                      callbacks=[early_stopping])
+                      callbacks=[early_stopping],
+                      shuffle=False)
   return history
 
 
@@ -375,7 +413,7 @@ multi_performance = {}
 
 multi_val_performance['Last_obs'] = last_baseline.evaluate(multi_window.val)
 multi_performance['Last_obs'] = last_baseline.evaluate(multi_window.test, verbose=0)
-multi_window.plot(last_baseline)
+multi_window.plot(last_baseline, title= 'Last observation (multivariate) 24 hours')
 
 # ----------------- instantiate and evaluate baseline model ------------------
 class RepeatBaseline(tf.keras.Model):
@@ -386,9 +424,9 @@ repeat_baseline = RepeatBaseline()
 repeat_baseline.compile(loss=tf.losses.MeanSquaredError(),
                         metrics=[tf.metrics.MeanAbsoluteError()])
 
-multi_val_performance['Repeat_24'] = repeat_baseline.evaluate(multi_window.val)
-multi_performance['Repeat_24'] = repeat_baseline.evaluate(multi_window.test, verbose=0)
-multi_window.plot(repeat_baseline)
+multi_val_performance['Repeat_win'] = repeat_baseline.evaluate(multi_window.val)
+multi_performance['Repeat_win'] = repeat_baseline.evaluate(multi_window.test, verbose=0)
+multi_window.plot(repeat_baseline, title = 'Previous window (multivariate) 24 hours')
 
 # ===================== Dense Neural Network =======================
 
@@ -397,7 +435,7 @@ multi_dense_model = tf.keras.Sequential([
     # Shape [batch, time, features] => [batch, 1, features]
     tf.keras.layers.Lambda(lambda x: x[:, -1:, :]),
     # Shape => [batch, 1, dense_units]
-    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(256, activation='relu'),
     # Shape => [batch, out_steps*features]
     tf.keras.layers.Dense(OUT_STEPS*num_features,
                           kernel_initializer=tf.initializers.zeros()),
@@ -410,7 +448,11 @@ history = compile_and_fit(multi_dense_model, multi_window)
 IPython.display.clear_output()
 multi_val_performance['Dense'] = multi_dense_model.evaluate(multi_window.val)
 multi_performance['Dense'] = multi_dense_model.evaluate(multi_window.test, verbose=0)
-multi_window.plot(multi_dense_model)
+multi_window.plot(multi_dense_model, title= 'Dense Neural Network (multivariate) 24 hours')
+plt.plot(history.history['loss'], label='train')
+plt.plot(history.history['val_loss'], label='validation')
+plt.title("Dense Loss 24 hours")
+plt.legend();
 
 # ================== Conv NN model =============================
 CONV_WIDTH = 3
@@ -418,7 +460,7 @@ multi_conv_model = tf.keras.Sequential([
     # Shape [batch, time, features] => [batch, CONV_WIDTH, features]
     tf.keras.layers.Lambda(lambda x: x[:, -CONV_WIDTH:, :]),
     # Shape => [batch, 1, conv_units]
-    tf.keras.layers.Conv1D(256, activation='relu', kernel_size=(CONV_WIDTH)),
+    tf.keras.layers.Conv1D(128, activation='relu', kernel_size=(CONV_WIDTH)),
     # Shape => [batch, 1,  out_steps*features]
     tf.keras.layers.Dense(OUT_STEPS*num_features,
                           kernel_initializer=tf.initializers.zeros()),
@@ -432,13 +474,17 @@ IPython.display.clear_output()
 
 multi_val_performance['Conv'] = multi_conv_model.evaluate(multi_window.val)
 multi_performance['Conv'] = multi_conv_model.evaluate(multi_window.test, verbose=0)
-multi_window.plot(multi_conv_model)
+multi_window.plot(multi_conv_model, title = "Convolutional Neural Network 24 hours")
+plt.plot(history.history['loss'], label='train')
+plt.plot(history.history['val_loss'], label='validation')
+plt.title("Conv loss 24 hours")
+plt.legend();
 
 # =================== run LSTm model ===========================
 multi_lstm_model = tf.keras.Sequential([
     # Shape [batch, time, features] => [batch, lstm_units]
     # Adding more `lstm_units` just overfits more quickly.
-    tf.keras.layers.LSTM(32, return_sequences=False),
+    tf.keras.layers.LSTM(32, activation = 'relu', return_sequences=False),
     # Shape => [batch, out_steps*features]
     tf.keras.layers.Dense(OUT_STEPS*num_features,
                           kernel_initializer=tf.initializers.zeros()),
@@ -452,17 +498,23 @@ IPython.display.clear_output()
 
 multi_val_performance['LSTM'] = multi_lstm_model.evaluate(multi_window.val)
 multi_performance['LSTM'] = multi_lstm_model.evaluate(multi_window.test, verbose=0)
-multi_window.plot(multi_lstm_model)
+multi_window.plot(multi_lstm_model, title = 'LSTM (multivariate) 24 hours')
+plt.plot(history.history['loss'], label='train')
+plt.plot(history.history['val_loss'], label='validation')
+plt.title("LSTM loss 24 hours")
+plt.legend();
 
 # -------------------plot evaluation metrics --------------
 
 # =============================================================================
 # plt.plot(history.history['loss'], label='train')
 # plt.plot(history.history['val_loss'], label='validation')
+# plt.title("loss")
 # plt.legend();
 # 
 # plt.plot(history.history['mean_absolute_error'], label='train')
 # plt.plot(history.history['val_mean_absolute_error'],label='validation')
+# plt.title('MAE')
 # plt.legend();
 # 
 # =============================================================================
@@ -476,6 +528,7 @@ metric_index = multi_lstm_model.metrics_names.index('mean_absolute_error')
 val_mae = [v[metric_index] for v in multi_val_performance.values()]
 test_mae = [v[metric_index] for v in multi_performance.values()]
 
+plt.title('MAE (multivariate) 24 hours')
 plt.bar(x - 0.17, val_mae, width, label='Validation')
 plt.bar(x + 0.17, test_mae, width, label='Test')
 plt.xticks(ticks=x, labels=multi_performance.keys(),
--- Analysis/final-lstm-univariate.py
+++ Analysis/final-lstm-univariate.py
@@ -48,12 +48,19 @@ df.isna().sum()
 df_input = df[['Datetime', 'cross_cafe', 'cloud_cover', 'humidity', 'precip_dur_past1h', 'precip_past1h', 'temp_dew',
                'temp_dry', 'temp_max_past1h', 'temp_mean_past1h', 'temp_min_past1h', 'wind_dir', 'wind_max_per10min_past1h',
                'wind_speed', 'wind_speed_past1h', 'sun_last1h_glob', 'pressure']]
+date_time = df_input.pop('Datetime')
+
+# outlier detection 
+#df_input.describe()
+# boxplot 
+#sns.boxplot(data=df_input)
+
 
 pd.set_option('display.max_columns', None) 
 # df_input.describe() # looks alright 
 
 # set Datetime as index
-df_input = df_input.set_index('Datetime')
+# df_input = df_input.set_index('Datetime')
 df_input.shape
 
 # replace NA values in dataframe 
@@ -65,6 +72,7 @@ df_input.fillna(method='ffill', inplace = True)
 # we will replace na's in cloud cover and wind_min with 0
 df_input['cloud_cover'].fillna(0, inplace = True)
 df_input.isna().sum()
+
 
 # data exploration 
 # add hour column
@@ -83,17 +91,19 @@ df_input['is_weekend'] = ((df_input.index.dayofweek) // 5 == 1).astype(float)
 # plot heatmap
 # df_input = df_input.reset_index()
 # get correlations
-df_input_corr = df_input.corr()
-# create mask
-mask = np.triu(np.ones_like(df_input_corr, dtype=np.bool))
-
-sns.heatmap(df_input_corr, mask=mask, annot=True, fmt=".2f", cmap='Blues',
-            vmin=-1, vmax=1, cbar_kws={"shrink": .8})
+# =============================================================================
+# df_input_corr = df_input.corr()
+# # create mask
+# mask = np.triu(np.ones_like(df_input_corr, dtype=np.bool))
+# 
+# sns.heatmap(df_input_corr, mask=mask, annot=True, fmt=".2f", cmap='Blues',
+#             vmin=-1, vmax=1, cbar_kws={"shrink": .8})
+# =============================================================================
 
 # drop values 
 df_input = df_input[['cross_cafe', 'cloud_cover', 'humidity', 'pressure',
-                     'temp_mean_past1h', 'wind_max_per10min_past1h', 'sun_last1h_glob',
-                     'hour', 'day_of_week', 'is_weekend']]
+                     'temp_mean_past1h', 'wind_max_per10min_past1h', 'sun_last1h_glob', 
+                     'hour', 'day_of_month', 'day_of_week', 'month', 'is_weekend']]
 df_input.info()
 
 
@@ -117,14 +127,40 @@ df_input.info()
 # =============================================================================
 
 
+# dataframe for univariate 
+# df_input = df_input['cross_cafe']
+
 # create final dataframe for LSTM
-# df_input = df_input[['cross_cafe', 'cloud_cover', 'humidity', 'pressure',
-#                      'temp_mean_past1h', 'wind_max_per10min_past1h', 'sun_last1h_glob',
-#                      'hour', 'day_of_week', 'is_weekend']]
-# df_input.info()
+df_input = df_input[['cross_cafe', 'cloud_cover', 'humidity', 'pressure',
+                     'temp_mean_past1h', 'wind_max_per10min_past1h', 'sun_last1h_glob']]
+df_input.info()
+    
+# create daily, weekly, and yearly signals 
+timestamp_s = date_time.map(datetime.datetime.timestamp)
+day = 24*60*60
+week = 24*60*60*7
+year = (365.2425)*day
+
+df_input['Day sin'] = np.sin(timestamp_s * (2 * np.pi / day))
+df_input['Day cos'] = np.cos(timestamp_s * (2 * np.pi / day))
+
+
+plt.plot(np.array(df_input['Day sin'])[:25])
+plt.plot(np.array(df_input['Day cos'])[:25])
+plt.xlabel('Time [h]')
+plt.title('Time of day signal')
+
+
+# df_input.to_csv('final_dataset.csv')
+
+# =============================================================================
+# ax = sns.violinplot(x='Column', y='Normalized', data=df_input)
+# _ = ax.set_xticklabels(df_input.keys(), rotation=90)
+# =============================================================================
+
 
 # dataframe for univariate 
-df_input = pd.DataFrame(df_input['cross_cafe'])
+df_input = df_input[['cross_cafe', 'Day sin', 'Day cos']]
 
 # ================================== Multi Step single shot forecasting models ========================
 
@@ -138,7 +174,7 @@ train_df = df_input[0:int(n*0.7)]
 val_df = df_input[int(n*0.7):int(n*0.9)]
 test_df = df_input[int(n*0.9):]
 
-num_features = df_input.shape
+num_features = df_input.shape[1]
 # ------------------------------------------ normalize data --------------------------
 
 # normalize the data before training
@@ -238,16 +274,18 @@ WindowGenerator.split_window = split_window
 # -------------------------- define plot function for evaluation -------------
 
 # plot window
-def plot(self, model=None, plot_col='cross_cafe', max_subplots=3):
+def plot(self, model=None, plot_col='cross_cafe', max_subplots=3, title = 'plot'):
   inputs, labels = self.example
   plt.figure(figsize=(12, 8))
   plot_col_index = self.column_indices[plot_col]
   max_n = min(max_subplots, len(inputs))
   for n in range(max_n):
     plt.subplot(max_n, 1, n+1)
+    plt.tight_layout(pad=1.0)
     plt.ylabel(f'{plot_col} [norm]')
     plt.plot(self.input_indices, inputs[n, :, plot_col_index],
              label='Inputs', marker='.', zorder=-10)
+    plt.title(title)
 
     if self.label_columns:
       label_col_index = self.label_columns_indices.get(plot_col, None)
@@ -336,16 +374,19 @@ WindowGenerator.example = example
 # ----------------------- define forecasting horizon --------------------------  
   
 # define forecasting length
-lags = 24
-OUT_STEPS = 24
-multi_window = WindowGenerator(input_width=lags, label_width=OUT_STEPS, shift=OUT_STEPS,
-                     label_columns=['cross_cafe'])
-multi_window
+lags = 168
+OUT_STEPS = 168
+multi_window = WindowGenerator(input_width=lags,
+                               label_width=OUT_STEPS,
+                               shift=OUT_STEPS,
+                               label_columns=['cross_cafe'])
+# multi_window.plot()
+# multi_window
 
 # ------------------- create compile and fit -------------------------------
 MAX_EPOCHS = 100
 
-def compile_and_fit(model, window, patience=5):
+def compile_and_fit(model, window, patience=10):
   early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss',
                                                     patience=patience,
                                                     mode='min')
@@ -356,7 +397,8 @@ def compile_and_fit(model, window, patience=5):
 
   history = model.fit(window.train, epochs=MAX_EPOCHS,
                       validation_data=window.val,
-                      callbacks=[early_stopping])
+                      callbacks=[early_stopping],
+                      shuffle=False)
   return history
 
 
@@ -374,7 +416,7 @@ multi_performance = {}
 
 multi_val_performance['Last_obs'] = last_baseline.evaluate(multi_window.val)
 multi_performance['Last_obs'] = last_baseline.evaluate(multi_window.test, verbose=0)
-multi_window.plot(last_baseline)
+multi_window.plot(last_baseline, title= 'Last observation (Sales) 168 hours')
 
 # ----------------- instantiate and evaluate baseline model ------------------
 class RepeatBaseline(tf.keras.Model):
@@ -385,9 +427,9 @@ repeat_baseline = RepeatBaseline()
 repeat_baseline.compile(loss=tf.losses.MeanSquaredError(),
                         metrics=[tf.metrics.MeanAbsoluteError()])
 
-multi_val_performance['Repeat_24'] = repeat_baseline.evaluate(multi_window.val)
-multi_performance['Repeat_24'] = repeat_baseline.evaluate(multi_window.test, verbose=0)
-multi_window.plot(repeat_baseline)
+multi_val_performance['Repeat_win'] = repeat_baseline.evaluate(multi_window.val)
+multi_performance['Repeat_win'] = repeat_baseline.evaluate(multi_window.test, verbose=0)
+multi_window.plot(repeat_baseline, title = 'Previous window (Sales) 168 hours')
 
 # ===================== Dense Neural Network =======================
 
@@ -396,7 +438,7 @@ multi_dense_model = tf.keras.Sequential([
     # Shape [batch, time, features] => [batch, 1, features]
     tf.keras.layers.Lambda(lambda x: x[:, -1:, :]),
     # Shape => [batch, 1, dense_units]
-    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(256, activation='relu'),
     # Shape => [batch, out_steps*features]
     tf.keras.layers.Dense(OUT_STEPS*num_features,
                           kernel_initializer=tf.initializers.zeros()),
@@ -409,7 +451,11 @@ history = compile_and_fit(multi_dense_model, multi_window)
 IPython.display.clear_output()
 multi_val_performance['Dense'] = multi_dense_model.evaluate(multi_window.val)
 multi_performance['Dense'] = multi_dense_model.evaluate(multi_window.test, verbose=0)
-multi_window.plot(multi_dense_model)
+multi_window.plot(multi_dense_model, title= 'Dense Neural Network (Sales) 168 hours')
+plt.plot(history.history['loss'], label='train')
+plt.plot(history.history['val_loss'], label='validation')
+plt.title("Dense Loss 168 hours (Sales)")
+plt.legend();
 
 # ================== Conv NN model =============================
 CONV_WIDTH = 3
@@ -417,7 +463,7 @@ multi_conv_model = tf.keras.Sequential([
     # Shape [batch, time, features] => [batch, CONV_WIDTH, features]
     tf.keras.layers.Lambda(lambda x: x[:, -CONV_WIDTH:, :]),
     # Shape => [batch, 1, conv_units]
-    tf.keras.layers.Conv1D(256, activation='relu', kernel_size=(CONV_WIDTH)),
+    tf.keras.layers.Conv1D(128, activation='relu', kernel_size=(CONV_WIDTH)),
     # Shape => [batch, 1,  out_steps*features]
     tf.keras.layers.Dense(OUT_STEPS*num_features,
                           kernel_initializer=tf.initializers.zeros()),
@@ -431,16 +477,20 @@ IPython.display.clear_output()
 
 multi_val_performance['Conv'] = multi_conv_model.evaluate(multi_window.val)
 multi_performance['Conv'] = multi_conv_model.evaluate(multi_window.test, verbose=0)
-multi_window.plot(multi_conv_model)
+multi_window.plot(multi_conv_model, title = "Convolutional Neural Network (Sales) 168 hours")
+plt.plot(history.history['loss'], label='train')
+plt.plot(history.history['val_loss'], label='validation')
+plt.title("Conv loss 168 hours (Sales)")
+plt.legend();
 
 # =================== run LSTm model ===========================
 multi_lstm_model = tf.keras.Sequential([
     # Shape [batch, time, features] => [batch, lstm_units]
     # Adding more `lstm_units` just overfits more quickly.
-    tf.keras.layers.LSTM(32, return_sequences=False),
+    tf.keras.layers.LSTM(32, activation = 'relu', return_sequences=False),
     # Shape => [batch, out_steps*features]
     tf.keras.layers.Dense(OUT_STEPS*num_features,
-                          kernel_initializer=tf.initializers.zeros()),
+                          kernel_initializer=tf.initializers.random_normal()),
     # Shape => [batch, out_steps, features]
     tf.keras.layers.Reshape([OUT_STEPS, num_features])
 ])
@@ -451,17 +501,23 @@ IPython.display.clear_output()
 
 multi_val_performance['LSTM'] = multi_lstm_model.evaluate(multi_window.val)
 multi_performance['LSTM'] = multi_lstm_model.evaluate(multi_window.test, verbose=0)
-multi_window.plot(multi_lstm_model)
+multi_window.plot(multi_lstm_model, title = 'LSTM (Sales) 168 hours')
+plt.plot(history.history['loss'], label='train')
+plt.plot(history.history['val_loss'], label='validation')
+plt.title("LSTM loss 168 hours (Sales)")
+plt.legend();
 
 # -------------------plot evaluation metrics --------------
 
 # =============================================================================
 # plt.plot(history.history['loss'], label='train')
 # plt.plot(history.history['val_loss'], label='validation')
+# plt.title("loss")
 # plt.legend();
 # 
 # plt.plot(history.history['mean_absolute_error'], label='train')
 # plt.plot(history.history['val_mean_absolute_error'],label='validation')
+# plt.title('MAE')
 # plt.legend();
 # 
 # =============================================================================
@@ -475,6 +531,7 @@ metric_index = multi_lstm_model.metrics_names.index('mean_absolute_error')
 val_mae = [v[metric_index] for v in multi_val_performance.values()]
 test_mae = [v[metric_index] for v in multi_performance.values()]
 
+plt.title('MAE (Sales) 168 hours')
 plt.bar(x - 0.17, val_mae, width, label='Validation')
 plt.bar(x + 0.17, test_mae, width, label='Test')
 plt.xticks(ticks=x, labels=multi_performance.keys(),
